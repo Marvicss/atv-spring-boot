1ef56eadc3f91e6edea5c6a4c76eeaca21ebf959
Marvicss <marconivictor15@gmail.com>
2025-03-17 23:00:29 -0300

    first commit

---
+++ aos-atv-spring/src/main/java/com/example/aos_atv_spring/AosAtvSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aos_atv_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AosAtvSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AosAtvSpringApplication.class, args);
+	}
+
+}
---
+++ aos-atv-spring/src/test/java/com/example/aos_atv_spring/AosAtvSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aos_atv_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AosAtvSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
